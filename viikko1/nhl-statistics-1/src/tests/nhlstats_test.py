e2928086407a736dc1dcf3136daf49e3232b1ad6
xcvbnmas <132822281+xcvbnmas@users.noreply.github.com>
2023-11-06 18:04:19 +0200

    viikko1

--- viikko1/nhl-statistics-1/src/tests/nhlstats_test.py
+++ viikko1/nhl-statistics-1/src/tests/nhlstats_test.py
@@ -14,9 +14,35 @@ class PlayerReaderStub:
 
 class TestStatisticsService(unittest.TestCase):
     def setUp(self):
-        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
         self.stats = StatisticsService(
             PlayerReaderStub()
         )
         
-    def 
+    def test_search(self):
+        result = self.stats.search("Lemieux")
+        self.assertEqual(result.name, "Lemieux")
+
+    def test_team(self):
+        result = self.stats.team("EDM")
+        self.assertEqual(len(result), 3)
+
+    def test_top(self):
+        result = self.stats.top(3)
+        self.assertEqual(len(result), 4)
+
+    def test_player(self):
+        result = self.stats.search("M")
+        self.assertIsNone(result)       
+
+    def test_team2(self):
+        result = self.stats.team("M")
+        self.assertEqual(len(result), 0)
+
+    def test_points(self):
+        player = Player("Test Player", "Test Team", 5, 3)
+        self.assertEqual(player.points, 8)
+
+    def test_str_(self):
+        player = Player("Test Player", "Test Team", 5, 3)
+        self.assertEqual(str(player), "Test Player Test Team 5 + 3 = 8")
+        
